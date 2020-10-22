ca5a26a4c1ae263b2c81295cf70c1f0163a05c53
Joon Kim <jkim527@gmail.com>
2020-10-22 23:10:23 +0900

    Add pseudocode and define method

---
+++ stock_picker.rb
@@ -0,0 +1,9 @@
+def stock_picker(arr)
+    for i in 0..arr.length
+
+    # iterate on i, beginning with arr[0], calculate highest positive change in price
+    # among highest positive changes in price for each i, find the i with the best highest change in price total, and find the j that yields that price
+    # let m be the best_buy and n be best_sell
+    best_buy = m
+    best_sell = n
+    return [best_buy, best_sell]
